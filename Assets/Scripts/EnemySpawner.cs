36c42072611ce5f16f30561e6f490a0a866685b2
elorenz1 <elorenz@ucsc.edu>
2022-01-25 15:32:08 -0800

    added Timer

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -14,17 +14,28 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private float bigEnemy1Interval = 10f;
 
+    private float count = 30;
+    public float enemyCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(spawnEnemy(enemy1Interval, enemy1Prefab));
         StartCoroutine(spawnEnemy(bigEnemy1Interval, bigEnemy1Prefab));
+
+        // now enemyCount has the number of monsters spawned
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
-        yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
-        StartCoroutine(spawnEnemy(interval, enemy));
+        int enemyNum = (int)Mathf.Floor(count / interval);
+        enemyCount += enemyNum;
+
+        for (int i = 0; i < enemyNum; i++)
+        {
+            yield return new WaitForSeconds(interval);
+            GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+            // StartCoroutine(spawnEnemy(interval, enemy));
+        }
     }
 }
